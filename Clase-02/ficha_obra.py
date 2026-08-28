4246ff3352f8c00e34851f8d23ea9904c4efae0c
Juanfra221436 <JFO221436@fcddis365.ort.edu.uy>
2026-08-28 10:36:27 -0300

    Primer paso

---
+++ Clase-02/ficha_obra.py
@@ -0,0 +1,11 @@
+titulo = input("Título: ")
+autor = input("Autor: ")
+anio = int(input("Año: "))
+ancho = float(input("Ancho (cm): "))
+alto = float(input("Alto (cm): "))
+
+
+
+# TODO: orientación
+# TODO: tamaño
+# TODO: mostrar la ficha
